47fc82b6fcd0889772458a39bf1491f2b941897d
Faraaz Mohiuddin <faraazmohiuddin@yahoo.co.in>
2021-08-20 17:12:55 +0530

    Commit from Git CMD

---
+++ sample2.py
@@ -0,0 +1,10 @@
+ Volume in drive C has no label.
+ Volume Serial Number is 5AAC-9EF8
+
+ Directory of C:\Users\Dell E5450\cmdtrial
+
+20-08-2021  17:11    <DIR>          .
+20-08-2021  17:11    <DIR>          ..
+20-08-2021  17:11                 0 sample2.py
+               1 File(s)              0 bytes
+               2 Dir(s)   6,774,960,128 bytes free
